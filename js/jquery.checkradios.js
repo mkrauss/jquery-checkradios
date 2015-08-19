fb9ad5e3c38fc768a10e68fd4a8a6571f187621a
Matthew Krauss <m.d.krauss@gmail.com>
2015-08-18 21:31:55 -0400

    Whole New World - initial rewrite

--- js/jquery.checkradios.js
+++ js/jquery.checkradios.js
@@ -1,370 +1,142 @@
 /*
-	CHECKRADIOS - jQuery plugin to allow custom styling of checkboxes
-	by Chris McGuckin (https://github.com/cosmicwheels)
-	
-	
-	License: MIT (http://opensource.org/licenses/MIT)
-	
-	
-	Credits:
-	---------------------------------------------------------------------------------
-	
-	icomoon (http://icomoon.io/)
-	
-	Please refer to the icomoon website for the license regarding their icons.
-	
-	---------------------------------------------------------------------------------
-	
-	Fontawesome (http://fortawesome.github.io/Font-Awesome/)
-	
-	Please refer to the fontawesome website for the license regarding their icons.
-	
-	---------------------------------------------------------------------------------
-	
-	Stephan Richter (https://github.com/strichter)
-	
-	Thanks to Stephan for pointing out and helping to add the triggering of events 
-	to help further mimic the checkboxes and radios as well as providing other
-	important bug fixes.
-	
-	---------------------------------------------------------------------------------
-	---------------------------------------------------------------------------------
-	
+    CHECKRADIOS - jQuery plugin to allow custom styling of checkboxes
+    by Chris McGuckin (https://github.com/cosmicwheels)
+
+
+    License: MIT (http://opensource.org/licenses/MIT)
+
+
+    Credits:
+    ---------------------------------------------------------------------------------
+
+    icomoon (http://icomoon.io/)
+
+    Please refer to the icomoon website for the license regarding their icons.
+
+    ---------------------------------------------------------------------------------
+
+    Fontawesome (http://fortawesome.github.io/Font-Awesome/)
+
+    Please refer to the fontawesome website for the license regarding their icons.
+
+    ---------------------------------------------------------------------------------
+
+    Stephan Richter (https://github.com/strichter)
+
+    Thanks to Stephan for pointing out and helping to add the triggering of events
+    to help further mimic the checkboxes and radios as well as providing other
+    important bug fixes.
+
+    ---------------------------------------------------------------------------------
+    ---------------------------------------------------------------------------------
+
 
 */
+
 (function ($) {
-
-
-	$.fn.checkradios = function (options) {
-
-		var $elements = this;
-
-		//Default Settings
-		var settings = $.extend({
-
-			checkbox: {
-
-				iconClass: 'icon-checkradios-checkmark'
-
-			},
-
-			radio: {
-
-				iconClass: 'icon-checkradios-circle'
-
-			},
-
-
-			onChange: function (checked, $element, $realElement) {}
-
-		}, options);
-
-
-		// Functionality
-		var form = {
-
-			checkbox: function ($checkbox) {
-				
-
-				var THIS = this;
-
-				//Make sure checkradios isnt already in use
-				if (!$checkbox.parent().hasClass('checkradios-checkbox')) {
-
-					//Get the elements classes
-					var classes = $checkbox.attr('class');
-
-					if (classes === undefined) {
-
-						classes = '';
-
-					}
-
-					//Wrap the input
-					var $item = $checkbox.wrap('<div class="checkradios-checkbox ' + classes + '"/>');
-
-					//Get the new checkbox
-					var $holder = $item.parent();
-
-					//Check if box is checked
-					if ($item.is(':checked')) {
-
-						THIS.checkboxEnable($item);
-
-					} else {
-
-						THIS.checkboxDisable($item);
-
-					}
-
-					//Add keyboard support
-					$checkbox.keypress(function (e) {
-
-						var key = e.keyCode;
-
-						//On enter/return or space
-						if ((key < 1) || (key == 13) || (key == 32)) {
-
-							$holder.click();
-
-						}
-
-					});
-
-
-					//Add tabbing support
-					$checkbox.on({
-
-						focusin: function () {
-
-							$holder.addClass('focus');
-
-						},
-
-						focusout: function () {
-
-							var $this = $(this);
-
-							setTimeout(function () {
-
-								if (!$this.is(':focus')) {
-
-									$holder.removeClass('focus');
-
-								}
-
-							}, 10);
-
-						}
-					});
-
-
-					$holder.mousedown(function () {
-
-						setTimeout(function () {
-
-							//Add focus
-							$checkbox.focus();
-
-						}, 10);
-
-					});
-
-
-					//Disable usual click functionality
-					$checkbox.click(function (e) {
-
-						e.stopPropagation();
-						e.preventDefault();
-
-					});
-
-
-					//On button click
-					$holder.click(function () {
-
-						//Add check
-						if ($item.is(':checked')) {
-
-							THIS.checkboxDisable($item);
-
-							//Callback
-							settings.onChange(false, $holder, $item);
-
-						} else {
-
-							THIS.checkboxEnable($item);
-
-							//Callback
-							settings.onChange(true, $holder, $item);
-
-						}
-
-						return false;
-
-					});
-
-				}
-
-			},
-
-
-			radio: function ($radio) {
-
-				var THIS = this;
-
-				//Make sure checkradios isnt already in use
-				if (!$radio.parent().hasClass('checkradios-radio')) {
-
-					//Get the elements classes
-					var classes = $radio.attr('class');
-
-					if (classes === undefined) {
-
-						classes = '';
-
-					}
-
-					//Wrap the input
-					var $item = $radio.wrap('<div class="checkradios-radio ' + classes + '"/>');
-
-					//Get the new checkbox
-					var $holder = $item.parent();
-
-					//Check if box is checked
-					if ($item.is(':checked')) {
-
-						THIS.radioEnable($item);
-
-					} else {
-
-						THIS.radioDisable($item);
-
-					}
-
-					//Add tabbing suppot
-					$radio.on({
-						
-						focusin: function () {
-							
-							//Add focus class
-							$holder.addClass('focus');
-							
-							
-							THIS.radioEnable($item);
-
-							//Get group Name
-							var radio_name = $item.attr('name');
-
-							var $group = $('input[name="' + radio_name + '"]');
-
-							//Set checked/unchecked for each element in group
-							$group.each(function () {
-								
-								if ($(this).is(':checked')) {
-									
-									THIS.radioEnable($(this));
-									//Callback
-									settings.onChange(true, $(this).parent(), $(this));
-									
-								} else {
-									
-									THIS.radioDisable($(this));
-									//Callback
-									settings.onChange(false, $(this).parent(), $(this));
-									
-								}
-
-							});
-
-						},
-
-						focusout: function () {
-							
-							var $this = $(this);
-
-							setTimeout(function () {
-								
-								if (!$this.is(':focus')) {
-									
-									$holder.removeClass('focus');
-									
-								}
-								
-							}, 10);
-							
-						}
-					});
-
-					$holder.click(function () {
-						
-						//Add focus
-						$radio.focus();
-						
-					});
-
-					//Disable usual click functionality
-					$radio.click(function (e) {
-						
-						e.stopPropagation();
-						e.preventDefault();
-						
-					});
-
-				}
-			},
-
-			/*Private*/
-
-			checkboxEnable: function ($checkbox) {
-				
-				$checkbox.parent().removeClass(settings.checkbox.iconClass);
-				$checkbox.parent().removeClass('unchecked');
-
-				$checkbox.parent().addClass(settings.checkbox.iconClass);
-				$checkbox.parent().addClass('checked');
-				$checkbox.prop('checked', true).trigger('change');
-				
-					
-			},
-
-			checkboxDisable: function ($checkbox) {
-				
-				$checkbox.parent().removeClass(settings.checkbox.iconClass);
-				$checkbox.parent().addClass('unchecked');
-				$checkbox.prop('checked', false).trigger('change');
-				
-				
-			},
-
-			radioEnable: function ($radio) {
-				
-				$radio.parent().removeClass(settings.radio.iconClass);
-				$radio.parent().removeClass('unchecked');
-
-				$radio.parent().addClass(settings.radio.iconClass);
-				$radio.parent().addClass('checked');
-				$radio.prop('checked', true).trigger('change');
-				
-				
-					
-			},
-
-			radioDisable: function ($radio) {
-				
-				$radio.parent().removeClass('checked');
-				$radio.parent().removeClass(settings.radio.iconClass);
-				$radio.parent().addClass('unchecked');
-				$radio.prop('checked', false).trigger('change');
-				
-				
-			}
-		};
-
-		//Loop through elements
-		$elements.each(function (i, val) {
-
-			var $this = $(this);
-
-			//Check for checkbox
-			if ($this.is("input[type=checkbox]")) {
-				
-				//Setup checkboxes
-				form.checkbox($this);
-				
+    var defaults = {
+        checkbox: {
+            facadeClass: 'checkradios-checkbox',
+            iconClass: 'icon-checkradios-checkmark'
+        },
+        radio: {
+            facadeClass: 'checkradios-radio',
+            iconClass: 'icon-checkradios-circle'
+        }
+    };
+
+    $.fn.checkradios = function (options) {
+        var settings = $.extend(defaults, options);
+
+        this.each(function () {
+			var element = $(this);
+
+			if (element.is("input[type=checkbox]")) {
+				initCheckbox(element, settings);
 			}
 
-			//Check for radio
-			if ($this.is("input[type=radio]")) {
-				
-				//Setup checkboxes
-				form.radio($this);
-				
+			else if (element.is("input[type=radio]")) {
+				initRadio(element, settings);
 			}
+        });
 
-			return this;
-		});
+        return this;
+    }
 
-	};
+    function initCheckbox(checkbox, settings) {
+	    var facade = $('<div/>')
+            .addClass(settings.checkbox.facadeClass)
+            .addClass(checkbox.attr('class'));
 
-}(jQuery));
+        checkbox.replaceWith(facade).appendTo(facade);
+
+        facade.on('click mousedown mouseup', function(event) {
+            event.target = checkbox[0];
+            checkbox.trigger(event);
+        });
+
+        facade.on('click', function() {
+            checkbox.focus();
+        });
+
+        checkbox.on('focus', function() {
+            facade.addClass('focus');
+        });
+
+        checkbox.on('blur', function() {
+            facade.removeClass('focus');
+        });
+
+        checkbox.on('change', function() {
+			facade.toggleClass(settings.checkbox.iconClass, checkbox.prop('checked'));
+        });
+
+        checkbox.on('click mousedown mouseup', function(event) {
+			event.stopPropagation();
+        });
+    }
+
+    function initRadio(radio, settings) {
+	    var facade = $('<div/>')
+            .addClass(settings.radio.facadeClass)
+            .addClass(radio.attr('class'));
+
+        var group = $('input:radio[name=' + radio.attr('name') + ']');
+
+        radio.replaceWith(facade).appendTo(facade);
+
+        facade.on('click mousedown mouseup', function(event) {
+            event.target = radio[0];
+            radio.trigger(event);
+        });
+
+        facade.on('click', function() {
+            radio.focus();
+        });
+
+        radio.on('focus', function() {
+            facade.addClass('focus');
+        });
+
+        radio.on('blur', function() {
+            facade.removeClass('focus');
+        });
+
+        radio.on('change', function() {
+            group.each(function() {
+                $(this).parent().toggleClass(settings.radio.iconClass, this.checked)
+            });
+        });
+
+        radio.on('click mousedown mouseup', function(event) {
+			event.stopPropagation();
+        });
+    }
+
+})(jQuery);
+
+$(document).ready(function () {
+    $('input:checkbox').checkradios();
+    $('input:radio').checkradios();
+});
+
